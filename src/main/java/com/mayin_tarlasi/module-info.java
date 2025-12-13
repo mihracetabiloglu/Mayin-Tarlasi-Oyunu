4ffb1df21c173637369b30a7a928f61c11873d3f
Mihrace <mihracetabiloglu@gmail.com>
2025-12-13 14:40:06 +0300

    fix(module): add javafx.graphics requirement to module-info

--- src/main/java/com/mayin_tarlasi/module-info.java
+++ src/main/java/com/mayin_tarlasi/module-info.java
@@ -1,8 +1,12 @@
 module com.mayin_tarlasi {
     // JavaFX kütüphanelerini kullanacağımızı belirtiyoruz
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media; // Müzik çalar eklemiştik
+
+    // Eğer uygulama native erişim gerektiren kütüphaneler kullanıyorsa,
+    // ek izinler veya --add-opens may be required at runtime.
 
     // FXML dosyasının ve Main sınıfının erişilebilir olmasını sağlıyoruz
     opens com.mayin_tarlasi to javafx.fxml;
